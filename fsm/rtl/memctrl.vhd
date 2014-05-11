e92e62b02dd9401b6e15473de825b92b9fb94ddc
GFudge <g.fudge-11@student.lboro.ac.uk>
2014-05-11 18:57:07 +0100

    Resized BRAM address port to width (32) from log2(locations) (10)

--- fsm/rtl/memctrl.vhd
+++ fsm/rtl/memctrl.vhd
@@ -36,7 +36,7 @@ component fsm
 		burst	:	in std_logic_vector(burstwidth - 1 downto 0);
 		din	:	in std_logic_vector(width - 1 downto 0);
 		dout 	:	out std_logic_vector(width - 1 downto 0);
-		maddr	:	buffer std_logic_vector(width - 1 downto 0);
+		maddr	:	out std_logic_vector(width - 1 downto 0);
 		rdy	:	out std_logic
 	);
 end component;
@@ -48,7 +48,7 @@ component bram_param
 	);
 	port (
 		clk	:	in std_logic;
-		addr	:	in std_logic_vector(log2(locations) - 1 downto 0);
+		addr	:	in std_logic_vector(width - 1 downto 0);
 		din 	:	in std_logic_vector(width - 1 downto 0);
 		dout	:	out std_logic_vector(width - 1 downto 0);
 		WE	: 	in std_logic
